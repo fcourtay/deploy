b8f7a9e58447ee48cc834bcc318417e08b0e3429
Florent Courtay <florent.courtay@sourcedgroup.com>
2018-12-28 11:01:12 -0500

    win10scripts

---
+++ WindowsAppxCleanup.ps1
@@ -0,0 +1,80 @@
+# Functions
+Function WriteLog {
+    [CmdletBinding()]
+    param (
+       [Parameter(Mandatory=$TRUE)][ValidateNotNullOrEmpty()][string]$LogFile,
+       [parameter(Mandatory=$TRUE)][ValidateNotNullOrEmpty()][string]$LineOfText
+     )
+    try {
+        Add-Content -Value "$(Get-Date) - $LineOfText" -LiteralPath $LogFile -ErrorAction Stop
+    }Catch [System.Exception] {
+        Write-Warning -Message "Unable to append log entry to $LogFile file"
+    }
+}
+
+$LogFile = $env:windir+"\Logs\DeploymentLogs\Script-RemovedUniversalApps.log"
+
+# Get a list of all apps
+WriteLog -LogFile $LogFile -LineOfText "Starting built-in AppxPackage and AppxProvisioningPackage removal process"
+$AppArrayList = Get-AppxPackage -PackageTypeFilter Bundle -AllUsers | Select-Object -Property Name, PackageFullName | Sort-Object -Property Name
+
+# White list of appx packages to keep installed
+$WhiteListedApps = @(
+    "Microsoft.StorePurchaseApp",
+    "Microsoft.WindowsCalculator",
+    "Microsoft.WindowsStore",
+    "Microsoft.Windows.Photos"
+    "Microsoft.PPIProjection"
+    "Microsoft.MSPaint"
+    "Microsoft.DesktopAppInstaller"
+    "Microsoft.WindowsCamera"
+    "Microsoft.WindowsMaps"
+)
+
+# Loop through the list of appx packages
+foreach ($App in $AppArrayList) {
+    # If application name not in appx package white list, remove AppxPackage and AppxProvisioningPackage
+    if (($App.Name -in $WhiteListedApps)) {
+        WriteLog -LogFile $LogFile -LineOfText "Skipping excluded application package: $($App.Name)"
+    }
+    else {
+        # Gather package names
+        $AppPackageFullName = Get-AppxPackage -Name $App.Name | Select-Object -ExpandProperty PackageFullName
+        $AppProvisioningPackageName = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -like $App.Name } | Select-Object -ExpandProperty PackageName
+
+        # Attempt to remove AppxPackage
+        if ($AppPackageFullName -ne $null) {
+            try {
+                WriteLog -LogFile $LogFile -LineOfText  "Removing application package: $($App.Name)"
+                Remove-AppxPackage -Package $AppPackageFullName -ErrorAction Stop | Out-Null
+            }
+            catch [System.Exception] {
+                WriteLog -LogFile $LogFile -LineOfText  "Removing AppxPackage failed: $($_.Exception.Message)"
+            }
+        }
+        else {
+            WriteLog -LogFile $LogFile -LineOfText  "Unable to locate AppxPackage for app: $($App.Name)"
+        }
+
+        # Attempt to remove AppxProvisioningPackage
+        if ($AppProvisioningPackageName -ne $null) {
+            try {
+                WriteLog -LogFile $LogFile -LineOfText  "Removing application provisioning package: $($AppProvisioningPackageName)"
+                Remove-AppxProvisionedPackage -PackageName $AppProvisioningPackageName -Online -ErrorAction Stop | Out-Null
+            }
+            catch [System.Exception] {
+                WriteLog -LogFile $LogFile -LineOfText  "Removing AppxProvisioningPackage failed: $($_.Exception.Message)"
+            }
+        }
+        else {
+            WriteLog -LogFile $LogFile -LineOfText "Unable to locate AppxProvisioningPackage for app: $($App.Name)"
+        }
+    }
+}
+
+Get-appxpackage -allusers *king.com.CandyCrush* | Remove-AppxPackage
+Get-appxprovisionedpackage –online | where-object {$_.packagename –like "*king.com.CandyCrush*"} | remove-appxprovisionedpackage –online
+
+
+
+WriteLog -LogFile $LogFile -LineOfText "Removal process completed"
---
+++ WindowsInstall.ps1
@@ -0,0 +1,78 @@
+Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted
+## Requesting Admin prompt
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){   
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
+## Variables
+$ChocolatelyGetName = "ChocolateyGet"
+$NuGetName = "NuGet"
+$ChocoPackagesToInstall = `
+    #"office365proplus", `
+    #"microsoft-teams", `
+    #"powerbi", `
+    "visualstudiocode", `
+    "googlechrome", `
+    "mremoteng", ` #remote desktop manager
+    "7zip", `
+    #"rsat", ` #Remote Admin tools
+    "git", `
+    "python", `
+    #"nmap", ` #network mapping tool
+    "sysinternals", ` #pack of internal MS tools
+    #"lightshot.install", ` #screenshots
+    #"picpick.portable", ` #screenshots and editing
+    #"screentogif", ` #screen recorder
+    #"pester", ` #powershell unit testing
+    #"steam", `
+    #"qbittorrent", `
+    #"curl", ` #bash cmd
+    #"wget", ` #bash cmd
+    #"sql-server-management-studio", `
+    #"logitechgaming", ` #Logitech Gaming software (for my G35 Headset)
+    "vlc"
+$StartTime = Get-Date
+Write-Host "[START] $StartTime"
+## Install NuGet first
+Install-PackageProvider -Name $NuGetName -MinimumVersion 2.8.5.201 -Force
+
+## Verify installed
+$NuGetInstalled = [bool](Get-PackageProvider | ?{$_.Name -eq $NuGetName}).count
+if($NuGetInstalled){
+    ## Continue
+
+    ## Install ChocolateyGet
+    Find-PackageProvider -Name $ChocolatelyGetName | Install-PackageProvider -Force
+
+    ## Validate Installed
+    $ChocoModule = Get-Module -ListAvailable -Name $ChocolatelyGetName
+    $ChocoGetInstalled = [bool]($ChocoModule).count
+    if($ChocoGetInstalled){
+        ## Continue
+        Import-Module $ChocoModule -Force
+        
+        ## Make sure prereqs are met
+        Get-Package -Force | Out-Null
+        foreach($pkg in $ChocoPackagesToInstall){
+            $pkgInstallStart = get-date
+            ## Install Programs
+            Install-Package -Name $pkg -Force
+            $pkgInstallEnd = get-date
+            Write-Host "[INSTALL TIME] $(New-TimeSpan -Start $pkgInstallStart -End $pkgInstallEnd)"
+        }
+    }
+    else {
+        throw "$ChocolatelyGetName installation could not be found"
+    }
+}
+else {
+    throw "$NuGetName installation could not be found"
+}
+
+##Refresh PATH
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+$EndTime = Get-Date
+$Diff = New-TimeSpan -Start $StartTime -End $EndTime
+Write-Host "[END] $EndTime`nTotal: $Diff"
